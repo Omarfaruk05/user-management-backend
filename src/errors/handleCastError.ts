37a014855dba96f75136a4e31d21aebc224e4e94
Omarfaruk05 <mdomarfrauk149518.com>
2023-06-17 00:06:58 +0600

    [Done]cast error

---
+++ src/errors/handleCastError.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { IGenericErrorMessage } from "../interfaces/error";
+import httpStatus from "http-status";
+
+const handleCastError = (error: mongoose.Error.CastError) => {
+  console.log(error);
+  const errors: IGenericErrorMessage[] = [
+    {
+      path: error.path,
+      message: "Invalide Error.",
+    },
+  ];
+  const statusCode = httpStatus.BAD_REQUEST;
+  return {
+    statusCode,
+    message: "Case Error",
+    errorMessage: errors,
+  };
+};
+
+export default handleCastError;
---
+++ src/errors/handleZodError.ts
@@ -0,0 +1,21 @@
+import { ZodError, ZodIssue } from "zod";
+import { IGenericErrorResponse } from "../interfaces/common";
+import { IGenericErrorMessage } from "../interfaces/error";
+import httpStatus from "http-status";
+
+const handleZodError = (error: ZodError): IGenericErrorResponse => {
+  const errors: IGenericErrorMessage[] = error.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue?.message,
+    };
+  });
+  const statusCode = httpStatus.BAD_REQUEST;
+  return {
+    statusCode,
+    message: "Validation Error.",
+    errorMessage: errors,
+  };
+};
+
+export default handleZodError;
