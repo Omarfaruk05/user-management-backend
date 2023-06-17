585d8101a1c02ccbbcc597901fe21ef198a497c3
Omarfaruk05 <mdomarfrauk149518.com>
2023-06-17 12:54:28 +0600

    [Done]interface and model for user

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,18 @@
+import { Model } from "mongoose";
+
+export type UserName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type IUser = {
+  phoneNumber: string;
+  role: string;
+  password: string;
+  name: UserName;
+  address: string;
+  budget: number;
+  income: number;
+};
+
+export type UserModel = Model<IUser, Record<string, unknown>>;
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+import { IUser, UserModel } from "./user.interface";
+
+const userSchema = new Schema<IUser, UserModel>({
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    required: true,
+    type: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  budget: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  income: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+export const User = model<IUser, UserModel>("User", userSchema);
