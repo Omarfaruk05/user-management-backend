a18fb083e7ee832823a906d84b4e67a521a9747d
Omarfaruk05 <mdomarfrauk149518.com>
2023-06-18 12:37:55 +0600

    [Done] create order and get orders

---
+++ src/app/modules/order/order.route.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import validateRequest from "../../middlewares/validateRequest";
+import { OrderValidation } from "./order.validation";
+import { OrderController } from "./order.controller";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  validateRequest(OrderValidation.createOrderZodSchema),
+  OrderController.createOrder
+);
+router.get("/", OrderController.getAllOrders);
+
+export const OrderRoutes = router;
